e38cf5c5a07a79619124599bf8fb98c684a258dd
Lucas Rodríguez <lucasrodriguezdepena@gmail.com>
2024-02-12 19:33:32 -0300

    update portfolio
    implement CV.pdf on CV page and add content to Services page.

--- src/components/pages/Curriculum.js
+++ src/components/pages/Curriculum.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 export const Curriculum = () => {
   return (
     <section>
       <article id="container-cv">
         <h3 className="heading">Curriculum Vitae</h3>
+        <iframe src="docs/CV.pdf" style={{width: "100%", height: "85vh"}}></iframe>
       </article>
     </section>
   )
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -5,6 +5,20 @@ export const Services = () => {
     <section>
       <article id="container-services">
         <h3 className="heading">Services</h3>
+        <ul className="flex flex-column">
+          <li>Desarrollo de Aplicaciones Web 🌐</li>
+          <li>Desarrollo de Aplicaciones Desktop 💻</li>
+          <li>Desarrollo de Front-End ⭐</li>
+          <li>Desarrollo de Back-End 🧠</li>
+          <li>Diseño Web 🔥</li>
+          <li>Gestión de Proyectos 🤝</li>
+          <li>Resolución de Problemas ✍</li>
+          <li>Administración de Bases de Datos 📦</li>
+          <li>Orquestación de Aplicaciones con Contenedores 🐳</li>
+          <li>Programación Shell Script 👾</li>
+          <li>Automatización de Tareas 🔁</li>
+          <li>Edición de imágenes 💥</li>
+        </ul>
       </article>
     </section>
   )
