a98d2a18b11147353c1ea73cb707ff76e755a41c
Lucas Rodríguez <lucasrodriguezdepena@gmail.com>
2024-09-30 20:17:57 -0300

    Update ProjectPage.js
    solución hardcodeada del bug del idioma de la descripción

--- src/components/pages/ProjectPage.js
+++ src/components/pages/ProjectPage.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { myProjects } from '../../data/myProjects';
 import * as SiIcons from "react-icons/si";
 import useEmblaCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
-import { useContext } from 'react';
 import { LanguageContext } from '../../contexts/LanguageContext';
 
 export const ProjectPage = () => {
@@ -24,47 +23,52 @@ export const ProjectPage = () => {
         setProject(project);
     }, []);
 
-    return (
-        <section>
-            <article id="container-project">
-                <h3 className="subheading">{project.name}</h3>
-                <div className="flex flex-row" id="project-mask-technologies">
-                    <div className="embla" ref={emblaRef}>
-                        <div className="embla__container">
-                            <div className="embla__slide"><img src={`/images/${project.id}.png`} /></div>
-                            <div className="embla__slide"><img src={`/images/${project.id}0.png`} /></div>
-                            <div className="embla__slide"><img src={`/images/${project.id}1.png`} /></div>
+    try {
+        return (
+            <section>
+                <article id="container-project">
+                    <h3 className="subheading">{project.name}</h3>
+                    <div className="flex flex-row" id="project-mask-technologies">
+                        <div className="embla" ref={emblaRef}>
+                            <div className="embla__container">
+                                <div className="embla__slide"><img src={`/images/${project.id}.png`} /></div>
+                                <div className="embla__slide"><img src={`/images/${project.id}0.png`} /></div>
+                                <div className="embla__slide"><img src={`/images/${project.id}1.png`} /></div>
+                            </div>
+                        </div>
+                        <div id="project-technologies">
+                            <ul className="technologies flex flex-column">
+                                {
+                                    project.technologies &&
+                                    project.technologies.map(technologie => {
+                                        const iconName = `Si${technologie.charAt(0).toUpperCase()}${technologie.slice(1)}`;
+                                        const IconComponent = SiIcons[iconName];
+                                        return <li key={technologie}>
+                                            {
+                                                IconComponent ? <IconComponent size={50} title={technologie} />
+                                                    : <span>{technologie}</span>
+                                            }
+                                        </li>
+                                    })
+                                }
+                            </ul>
                         </div>
                     </div>
-                    <div id="project-technologies">
-                        <ul className="technologies flex flex-column">
-                            {
-                                project.technologies &&
-                                project.technologies.map(technologie => {
-                                    const iconName = `Si${technologie.charAt(0).toUpperCase()}${technologie.slice(1)}`;
-                                    const IconComponent = SiIcons[iconName];
-                                    return <li key={technologie}>
-                                        {
-                                            IconComponent ? <IconComponent size={50} title={technologie} />
-                                                : <span>{technologie}</span>
-                                        }
-                                    </li>
-                                })
-                            }
-                        </ul>
+                    <div className="text-box">
+                        <p>{project.description[language]}</p>
+                        <p>{project.descriptionLong[language]}</p>
+                        <p className="fit-content">{project.details[language]}</p>
+                        <p className="highlight-secondary fit-content">{texts.projectRole} {project.categories}.</p>
                     </div>
-                </div>
-                <div className="text-box">
-                    <p>{project.description[language]}</p>
-                    <p>{project.descriptionLong[language]}</p>
-                    <p className="fit-content">{project.details[language]}</p>
-                    <p className="highlight-secondary fit-content">{texts.projectRole} {project.categories}.</p>
-                </div>
-                <a href={project.url} target="_blank" className="button-secondary">{texts.projectSecondaryButton}</a>
-                {
-                    project.tryUrl !== "" && <a href={project.tryUrl} target="_blank" className="button-primary" style={{marginLeft: "30px"}}>{texts.projectPrimaryButton}</a>
-                }
-            </article>
-        </section>
-    )
+                    <a href={project.url} target="_blank" className="button-secondary">{texts.projectSecondaryButton}</a>
+                    {
+                        project.tryUrl !== "" && <a href={project.tryUrl} target="_blank" className="button-primary" style={{ marginLeft: "30px" }}>{texts.projectPrimaryButton}</a>
+                    }
+                </article>
+            </section>
+        )
+    } catch (e) {
+        console.log(e);
+        return <></>
+    }
 }
